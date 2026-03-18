026425f07098df13f69a840f5fd8a97725ccdf7f
AMEER <ameer.ad.eltaib@gmail.com>
2026-03-18 13:31:19 +0545

    Change file reading to use pipe method
    Refactor file reading method to use pipe for streaming.

--- Nodejs/streams.js
+++ Nodejs/streams.js
@@ -1,3 +1,5 @@
+//test-file.txt : very long file that is heave
+
 //How to read very long file using the usual way and streams?
 //Streams are the simple way say when you watch You Tube and the video 
 //  upload to you chunk by chunck. Notice the loading-bar
@@ -17,14 +19,19 @@ server.on('request', (req,res) => {
     // })
 
     //solution 2 using streams
-    const readable = fs.createReadStream('./test-file.txt')
-    readable.on('data', chunck => {
-        res.write(chunck)
-    })
+    // const readable = fs.createReadStream('./test-file.txt')
+    // readable.on('data', chunck => {
+    //     res.write(chunck)
+    // })
 
-    readable.on('end', ()=>{
-        res.end()
-    })
+    // readable.on('end', ()=>{
+    //     res.end()
+    // })
+
+    //solution3 using pipe
+    const readable = fs.createReadStream('./test-file.txt')
+    readable.pipe(res)
+    //readableSource.pipe(writableDestination)
 })
 
 //Note: data,end are event listeners sets by Node community. the callback in data accept the {chunck} of the file or the video
