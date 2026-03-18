b061108daac52fe1bab5580ad024ff22dca08898
AMEER <ameer.ad.eltaib@gmail.com>
2026-03-18 13:25:52 +0545

    Lesson3 Streams

---
+++ Nodejs/streams.js
@@ -0,0 +1,35 @@
+//How to read very long file using the usual way and streams?
+//Streams are the simple way say when you watch You Tube and the video 
+//  upload to you chunk by chunck. Notice the loading-bar
+
+const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer()
+
+server.on('request', (req,res) => {
+    //solution 1
+    // fs.readFile('./test-file.txt', (err,data)=>{
+    //     if(err){
+    //         console.log(err)
+    //     }
+    //     res.end(data)
+    // })
+
+    //solution 2 using streams
+    const readable = fs.createReadStream('./test-file.txt')
+    readable.on('data', chunck => {
+        res.write(chunck)
+    })
+
+    readable.on('end', ()=>{
+        res.end()
+    })
+})
+
+//Note: data,end are event listeners sets by Node community. the callback in data accept the {chunck} of the file or the video
+//the listen event of end must be in the code
+
+server.listen(8000, 'localhost', ()=>{
+    console.log('listening ...')
+})
